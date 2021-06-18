179ba6b684597d1389529a521c6acb1fb152218a
Alex <gamerboy@gmail.com>
2021-06-17 22:19:36 -0500

    modificaciones al codigo respecto a Maps SDK

--- app/src/main/java/com/example/gpscovid_semaforo/MapsActivity.java
+++ app/src/main/java/com/example/gpscovid_semaforo/MapsActivity.java
@@ -5,14 +5,12 @@ import android.util.Log;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
-/**
- *     Contraseña de la llave: semaforocovid
- *     Nombre de la llave my-key.jks
- */
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private String LOG_TAG;
@@ -33,7 +31,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 
