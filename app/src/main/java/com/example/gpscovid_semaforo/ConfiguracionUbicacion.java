abaefb4ba2279d3929ed7d4a5b36a05e7eda6631
Alex <gamerboy@gmail.com>
2021-07-20 21:00:22 -0500

    agregada clase de notificaciones y clase de ubicacion al igual que interfaz

---
+++ app/src/main/java/com/example/gpscovid_semaforo/ConfiguracionUbicacion.java
@@ -0,0 +1,14 @@
+package com.example.gpscovid_semaforo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracionUbicacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion_ubicacion);
+    }
+}
