355279fdeb6e71dc23bb0f3b79101b996e86079c
Alex <gamerboy@gmail.com>
2021-08-21 01:03:05 -0500

    Cambios en el codigo de ubicacion

--- app/src/main/java/com/example/gpscovid_semaforo/MapaMapBox.java
+++ app/src/main/java/com/example/gpscovid_semaforo/MapaMapBox.java
@@ -3,12 +3,20 @@ package com.example.gpscovid_semaforo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -17,7 +25,9 @@ import com.mapbox.mapboxsdk.maps.Style;
 public class MapBoxMap extends AppCompatActivity {
 
     private MapView mapView;
+    private MapboxMap mBoxMap;
     AbrirPoligonos abrirPoligonosMapa = new AbrirPoligonos();
+    MainActivity mActivity = new MainActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +56,55 @@ public class MapBoxMap extends AppCompatActivity {
                         mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
                         abrirPoligonosMapa.abrirPoligonos(style);
+                        enableLocationComponent(style);
                     }
                 });
             }
         });
     }//Fin de onCreate
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        mActivity.checkAndRequestPermissions();
+            // Get an instance of the component
+            LocationComponent locationComponent = mBoxMap.getLocationComponent();
+
+            // Set the LocationComponent activation options
+            LocationComponentActivationOptions locationComponentActivationOptions =
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                            .useDefaultLocationEngine(false)
+                            .build();
+
+            // Activate with the LocationComponentActivationOptions object
+            locationComponent.activateLocationComponent(locationComponentActivationOptions);
+
+            // Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+
+            initLocationEngine();
+    }
+
+    /**
+     * Set up the LocationEngine and the parameters for querying the device's location
+     */
+    @SuppressLint("MissingPermission")
+    private void initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this);
+
+        LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+                .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+
+        locationEngine.requestLocationUpdates(request, callback, getMainLooper());
+        locationEngine.getLastLocation(callback);
+    }
 
     @Override
     public void onResume() {
